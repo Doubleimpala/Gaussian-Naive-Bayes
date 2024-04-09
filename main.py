3aabd8e22e4937eb48e92f98a2459b80e0d91546
Tanmay <82619667+Doubleimpala@users.noreply.github.com>
2024-04-09 12:37:34 +0000

    Need to fix some probabilities and get help.

--- main.py
+++ main.py
@@ -10,17 +10,31 @@ data = pd.read_csv('data.txt',sep = ' ', header=None)
 #print(data)
 data = np.array(data)
 x, y = data[:, 1], data[:, 2]
-
+classes = data[:, 0]
+classArray = np.array(classes)
 meanx = np.mean(x)
-variancex = np.standardeviation(x)**2
+variancex = np.std(x)**2
+print('Input x')
 vx = input()
+print('Input y')
 vy = input()
-meany = 
-variancey = 
+meany = np.mean(y)
+variancey = np.std(y)**2
+classProbs = computeClassProbs(vx, vy, meanx, variancex, meany, variancey, classArray)
+print(classProbs)
 
-pvgivenc_x = pgivenc(vx,variancex,meanx)
-pvgivenc_y = pgivenc(vy,variancey,meany)
+def computeClassProbs(vx, vy, meanx, variancex, meany, variancey, classArray):
+  for o in range(len(classArray)):
+    indices = [i for i, x in enumerate(classArray) if x == o]
+    priorProb = len(indices)/len(classArray)
+    likelyhood_x = pgivenc(vx,variancex,meanx)
+    likelyhood_y = pgivenc(vy,variancey,meany)
+    evidenceProb_x = 1#P(Xi)
+    evidenceProb_y = 1#P(Xi)
+    #This is wrong. Fix.
+    classProbs[o] = (priorProb)
+  return classProbs
 
-
+#Fix this function. I think it requires both dimensions to work? x and y?
 def pgivenc(v,variance,mean):
-  return (1/(2*math.pi*variance**2))*exp((-(v-mean)**2)/(2*(variance**2)))
+  return (1/(2*math.pi*variance**2))*((math.e)**((-(v-mean)**2)/(2*(variance**2))))
